4cc77b60a2200a873054c9b0d6da0e25af03c355
b04902013 <b04902013@ntu.edu.tw>
2017-02-23 23:10:10 +0800

    hw0

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python2
 import sys, numpy
-A, B = [ numpy.loadtxt(fn, delimiter=',') for fn in sys.argv[1:3] ]
-numpy.savetxt('ans_one.txt', numpy.sort(numpy.matmul(A, B)), fmt='%d')
+A, B = [ numpy.loadtxt(fn, delimiter=',', ndmin=2) for fn in sys.argv[1:3] ]
+numpy.savetxt('ans_one.txt', numpy.sort(numpy.matmul(A, B)), fmt='%d', delimiter='\n')
